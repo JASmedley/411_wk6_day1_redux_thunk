0db4bb30466c2c2696f83ab5c318469b8374acf9
JASmedley <jasmine.a.smedley@gmail.com>
2024-02-13 20:48:15 -0600

    first commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import {Button, IconButton} from "@mui/material";
+import styles from "./import.module.css"
+import { MoreVert } from "@mui/icons-material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+
+
+const rows = [];
 
 const Import = (props) => {
-    // fill out this component
 
-    return (
-        <p>Import Component</p>
-    )
-}
+    console.log(props)
 
-export default Import
+  return (
+    <>
+    <section className={styles.grid}>
+      <h2>Import Component</h2>
+      <Button onClick={()=> props.fetchMakes()} className={styles.button} variant="contained" color="primary" >Import</Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell >MAKES</TableCell>
+              <TableCell align="right">ACTIONS</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.makes.map((row,index) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.MakeId}</TableCell>
+                <TableCell>{row.MakeName}</TableCell>
+                <TableCell align="right">
+                <IconButton aria-label="delete" onClick={()=> props.deleteMake(index)}>
+                <DeleteIcon />
+                </IconButton>
+                </TableCell>
+
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      </section>
+    </>
+  );
+};
+
+export default Import;
